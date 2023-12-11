fce80fcaedb0d9ff09c23a9b4607ea93e92ec3a0
Nilay Nagar <45895008+nilzzzz19@users.noreply.github.com>
2023-12-10 20:54:38 -0800

    more tonic

--- a-gin-server/server.go
+++ a-gin-server/server.go
@@ -1,19 +1,42 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	//fmt.Println("Hello World")
-	r := gin.Default() //
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run(":8001") // listen and serve on
+type Person struct {
+	Name         string
+	Age          int
+	IsInterested bool
+}
 
-	//change port
+func main() {
+
+	p := Person{Name: "john", Age: 20, IsInterested: true}
+
+	fmt.Printf(p.Name)
+
+	//fmt.Println("Hello World")
+	r := gin.Default()
+	// Load HTML files. Use LoadHTMLGlob if you have multiple HTML files.
+	r.LoadHTMLFiles("./frontend/index.html")
+
+	// Set up a route to serve the HTML file.
+	r.GET("/index", func(c *gin.Context) {
+		c.HTML(200, "index.html", gin.H{})
+	})
+
+	// Serve static files. The URL path is "/static" and files are served from the "./frontend" directory.
+	// Adjust the paths according to your directory structure.
+	r.Static("/static", "./frontend")
+
+	// Start the server on port 8001
+	r.Run(":8001")
+	r.Static("/static", "./frontend")
+
+	// Start the server on port 8001
+	r.Run(":8001")
 
 }
